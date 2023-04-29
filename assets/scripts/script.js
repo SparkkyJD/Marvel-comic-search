eee5e23bcbed4b98126ef451f9ef20e6e00329cf
SparkkyJD <127361245+SparkkyJD@users.noreply.github.com>
2023-04-29 17:25:49 -0400

    polished js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,43 +1,32 @@
-// JavaScript Document
-let resultsitem = document.getElementById('resultsItem');
-let searchForm = document.getElementById('searchForm');
-let searchInput = document.getElementById('searchInput');
-
-searchForm.addEventListener('submit', (event)=> {
+let searchResults = document.getElementById('searchResults');
+let searchBar = document.getElementById('searchBar');
+let searchInquiry = document.getElementById('searchInquiry');
+searchBar.addEventListener('submit', (event)=> {
   event.preventDefault();
-  displaySearchResults(searchInput.value);
+  fetchData(searchInquiry.value);
 })
-
-function displaySearchResults(x){
+function fetchData(x){
   let url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=1&srsearch=${x}`;
   fetch(url)
     .then(function(response) {
-      console.log(response)
       return (response.json());
     })
     .then(function(data){
-      let resultsArray = data.query.search;
-      resultsOnPage(resultsArray);
-      console.log(data)
+      let resultsData = data.query.search;
+      displayResults(resultsData);
+	  console.log(data);
     })
-    .catch(function () {
-      console.log('An error occurred');
-    });
 }
-
-function resultsOnPage(myArray){
-  resultsitem.innerHTML = '';
-  let item = myArray[0];
+function displayResults(Array){
+  let item = Array[0];
   let itemTitle = item.title;
   let itemSnippet = item.snippet;
+  let itemTimeStamp = new Date(item.timestamp).toLocaleString();
   let itemUrl = encodeURI(`https://en.wikipedia.org/wiki/${item.title}`);
-  resultsitem.insertAdjacentHTML('beforeend',
-    `<div class="resultItem">
-      <h2 class="resultTitle">
-        <a href="${itemUrl}" target="_blank" rel="noopener">${itemTitle}</a>
-      </h2>
-      <p class="resultSnippet"><a href="${itemUrl}"  target="_blank" rel="noopener">
-      ${itemSnippet}</a></p>
-    </div>`
-  );
+  searchResults.insertAdjacentHTML('beforeend',
+    `<aside class="resultItem">
+	  <h1 class="itemTimeStamp">${itemTimeStamp}</a></h1>
+      <h2 class="itemTitle"><a href="${itemUrl}" target="_blank" rel="noopener">${itemTitle}</a></h2>
+      <p class="itemSnippet"><a href="${itemUrl}"  target="_blank" rel="noopener">${itemSnippet}</a></p>
+    </aside>`);
 }

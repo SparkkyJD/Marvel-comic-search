867fdd2a4967ca3503d00b4cdb68f088d0531832
SparkkyJD <127361245+SparkkyJD@users.noreply.github.com>
2023-04-30 15:39:37 -0400

    Update script.js
    added local storage function that will append search history to page when refreshed.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,10 +1,40 @@
 let searchResults = document.getElementById('searchResults');
 let searchBar = document.getElementById('searchBar');
 let searchInquiry = document.getElementById('searchInquiry');
-searchBar.addEventListener('submit', (event)=> {
+
+// create clear button element
+let clearButton = document.createElement('button');
+  clearButton.textContent = 'Clear History';
+
+// add click event listener to clear button
+clearButton.addEventListener('click', function() {
+  localStorage.clear();
+
+// remove history list from HTML
+  document.getElementById('history').innerHTML = ''; 
+});
+// append clear button to HTML
+  document.getElementById('searchBar').appendChild(clearButton);
+
+// call getLocalStorage() once on page load
+getLocalStorage();
+function getLocalStorage() {
+  let historyList = document.createElement('ul');
+    for (let i = 0; i < localStorage.length; i++) {
+      let key = localStorage.key(i);
+      let value = localStorage.getItem(key);
+      let listItem = document.createElement('li');
+      listItem.textContent = `${key}: ${value}`;
+      historyList.appendChild(listItem);
+    }
+  document.getElementById('history').appendChild(historyList);
+}
+
+  searchBar.addEventListener('submit', (event)=> {
   event.preventDefault();
   fetchData(searchInquiry.value);
 })
+// fetch data from wiki api 
 function fetchData(x){
   let url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=1&srsearch=${x}`;
   fetch(url)
@@ -17,16 +47,18 @@ function fetchData(x){
 	  console.log(data);
     })
 }
+// pull data from array and append to html
 function displayResults(Array){
   let item = Array[0];
   let itemTitle = item.title;
   let itemSnippet = item.snippet;
   let itemTimeStamp = new Date(item.timestamp).toLocaleString();
+  localStorage.setItem(itemTitle, "");
   let itemUrl = encodeURI(`https://en.wikipedia.org/wiki/${item.title}`);
   searchResults.insertAdjacentHTML('beforeend',
-    `<aside class="resultItem">
-	  <h1 class="itemTimeStamp">${itemTimeStamp}</a></h1>
+    `<article class="resultItem">
+	    <h1 class="itemTimeStamp">${itemTimeStamp}</h1>
       <h2 class="itemTitle"><a href="${itemUrl}" target="_blank" rel="noopener">${itemTitle}</a></h2>
-      <p class="itemSnippet"><a href="${itemUrl}"  target="_blank" rel="noopener">${itemSnippet}</a></p>
-    </aside>`);
+      <p class="itemSnippet">${itemSnippet}</p>
+    </article>`);
 }

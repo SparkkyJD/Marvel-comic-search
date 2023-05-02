51621003fbcaf97d1a716b66b08a7251658b7407
Jack <jebulger1003@gmail.com>
2023-05-02 16:58:25 -0400

    data from marvel api is now being dynamically added to the page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,10 @@
 // ToDo: Get api keys and store them into variables
 
 // ToDo: Use selectors to store the html elements into variables
-var searchInput = document.querySelector('#marvel-search');
-var searchButton = document.querySelector('#marvel-search-button');
-var resultList = document.querySelector('#test-list');
+// var searchInput = document.querySelector('#marvel-search');
+// var searchButton = document.querySelector('#marvel-search-button');
+// var resultList = document.querySelector('#test-list');
+var searchResultEl = document.querySelector('.is-flex-wrap-wrap');
 
 // ToDo: Write a function to obtain the user search on submit
 
@@ -15,7 +16,7 @@ var resultList = document.querySelector('#test-list');
 
 function getMarvelData(event) {
     event.preventDefault(); //ensures input is processed
-    searchInput = $('#marvel-search').val();
+    var searchInput = $('#marvel-search').val();
 
     getMarvelInputData(searchInput);
 
@@ -60,6 +61,7 @@ function getMarvelInputData(searchInput) {
         .then((response) => response.json())
         .then(result => {
             console.log("Marvel Data ", result);
+            displayResults(result);
         })
 }
 
@@ -69,6 +71,84 @@ function getMarvelInputData(searchInput) {
 // ToDo: Write a function to display the search results onto the page
 
 $('#marvel-search-button').on("click", getMarvelData);
+
+// Function to dynamically add search result cards based on search result length
+// All that is needed for this now is to add the result data in their corresponding spots
+function displayResults(result) {
+    console.log(result.data.results);
+    // looping through and creating cards based on the result length
+    for (var i = 0; i < result.data.results.length; i++) {
+        var subTitleP = document.createElement('p');
+        subTitleP.classList.add('subtitle', 'is-6');
+
+        var titleP = document.createElement('p');
+        titleP.classList.add('title', 'is-5', 'pb-2');
+
+        var contentDiv = document.createElement('div');
+        contentDiv.classList.add('content');
+
+        var cardContent = document.createElement('div');
+        cardContent.classList.add('card-content', 'pt-3');
+
+        var imgTag = document.createElement('img');
+        imgTag.src = ''; 
+
+        var cardFigure = document.createElement('figure');
+        cardFigure.classList.add('image', 'is-is-2by3');
+
+        var cardImage = document.createElement('div');
+        cardImage.classList.add('card-image', 'px-5', 'pt-5');
+
+        var iTagEl = document.createElement('i');
+        iTagEl.classList.add('fa-sharp', 'fa-solid', 'fa-bookmark');
+
+        var cardSaveButton = document.createElement('button');
+        cardSaveButton.classList.add('save-btn', 'c-btn');
+
+        var itemCard = document.createElement('div');
+        itemCard.classList.add('card', 'search-item');
+
+        var resultCard = document.createElement('div');
+        resultCard.classList.add('column', 'is-one-fifth');
+
+        // Still need to finish generating content for the modal
+        // But this is for the data on the result cards
+        titleP.textContent = result.data.results[i].title;
+        if (result.data.results[i].images.length < 1) {
+            imgTag.src = './assets/images/sorry-cannot-be-found.png';
+        } else {
+            imgTag.src = result.data.results[i].images[0].path + '.jpg';
+        }
+
+        if (result.data.results[i].creators.available < 1) {
+            subTitleP.textContent = 'Creator not credited';
+        } else {
+            subTitleP.textContent = result.data.results[i].creators.items[0].name;
+        }
+
+        // Appending to match the demo card structure
+        contentDiv.appendChild(titleP);
+        contentDiv.appendChild(subTitleP);
+        cardContent.appendChild(contentDiv);
+
+        cardFigure.appendChild(imgTag);
+        cardImage.appendChild(cardFigure);
+
+        cardSaveButton.appendChild(iTagEl);
+
+        itemCard.appendChild(cardSaveButton);
+
+        itemCard.appendChild(cardImage);
+        itemCard.appendChild(cardContent);
+
+        resultCard.appendChild(itemCard);
+        searchResultEl.appendChild(resultCard);
+
+        // Need to figure out how to differentiate the generated save buttons
+        // so that each button will save only its corresponding content
+        // cardSaveButton.addEventListener('click', addToLocalStorage);
+    }
+}
 
 // ToDo: Create a function to display a modal asking the user for location parameters
 // then pass those parameters into the api call
@@ -80,20 +160,43 @@ $('#marvel-search-button').on("click", getMarvelData);
 // maybe by storing the user's location? Or maybe by storing their previous
 // searches and displaying them to the page for shortcut links?
 
+// Just temporary, this query selector will be replaced by the dynamically generated
+// cardSaveButtons
+var saveButton = document.querySelector('.save-btn');
+
+var collectionEntry = {
+    writer: "",
+    datePublished: "",
+    title: "",
+    penciller: "",
+    coverArtist: ""
+  };
+
+function addToLocalStorage() {
+    var storedCollection = JSON.parse(localStorage.getItem('userCollection')) || [];
+    collectionEntry.index++; 
+    storedCollection.push(collectionEntry);
+    localStorage.setItem('userCollection', JSON.stringify(storedCollection));
+}
+
+// Just temporary, the dynamically generated buttons will have event listeners to take this
+// placeholder's place
+saveButton.addEventListener('click', addToLocalStorage);
+
 // ToDo: Add event listener to submit button
 
 
-const galleryItem = document.querySelector(".gallery-item");
-const showModal = document.querySelector(".modal");
-const modalBackground = document.querySelector(".modal-background");
+// const galleryItem = document.querySelector(".gallery-item");
+// const showModal = document.querySelector(".modal");
+// const modalBackground = document.querySelector(".modal-background");
 
-galleryItem.addEventListener('click', function () {
-    showModal.classList.add('is-active');
-    console.log('hello world');
-  });
-  modalBackground.addEventListener('click', function () {
-    showModal.classList.remove('is-active');
-  });
+// galleryItem.addEventListener('click', function () {
+//     showModal.classList.add('is-active');
+//     console.log('hello world');
+//   });
+//   modalBackground.addEventListener('click', function () {
+//     showModal.classList.remove('is-active');
+//   });
 
 
 
